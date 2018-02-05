1862bb1c56d18d048244aac400ba3cf4170a60ec
demidemid <iamdemid@gmail.com>
2018-02-06 02:04:47 +0300

    правки замечаний по тревису

--- js/stat.js
+++ js/stat.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict';
 
 var CLOUD_WIDTH = 420;
 var CLOUD_HEIGHT = 270;
@@ -30,31 +30,31 @@ var getMaxElement = function(arr) {
 };
 
 window.renderStatistics = function(ctx, names, times) {
-  renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, "rgba(0, 0, 0, 0.3)");
-  renderCloud(ctx, CLOUD_X, CLOUD_Y, "#fff");
+  renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0, 0, 0, 0.3)');
+  renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff');
 
-  ctx.fillStyle = "#000";
-  ctx.font="16px PT Mono";
-  ctx.fillText("Ура вы победили!", CLOUD_X + GAP * 2, CLOUD_Y + GAP * 3);
-  ctx.fillText("Список результатов:", CLOUD_X + GAP * 2, CLOUD_Y + GAP * 5);
+  ctx.fillStyle = '#000';
+  ctx.font='16px PT Mono';
+  ctx.fillText('Ура вы победили!', CLOUD_X + GAP * 2, CLOUD_Y + GAP * 3);
+  ctx.fillText('Список результатов:', CLOUD_X + GAP * 2, CLOUD_Y + GAP * 5);
 
   var maxTime = getMaxElement(times);
 
   for (var i = 0; i < names.length; i++) {
     var columnHeight = (BAR_HEIGHT * times[i].toFixed(0)) / maxTime;
 
-    if (names[i] == "Вы") {
-      var color = "rgba(255, 0, 0, 1)";
-    } else if (names[i] == "Кекс") {
-      color = "blue";
+    if (names[i] === 'Вы') {
+      var color = 'rgba(255, 0, 0, 1)';
+    } else if (names[i] === 'Кекс') {
+      color = 'blue';
     } else {
-      color = "grey";
+      color = 'grey';
     }
 
     ctx.fillText(times[i].toFixed(0), COLUMN_BLOCK_X + (BAR_GAP + BAR_WIDTH) * i, COLUMN_BLOCK_Y - (columnHeight + GAP * 3));
     ctx.fillStyle = color;
     ctx.fillRect(COLUMN_BLOCK_X + (BAR_GAP + BAR_WIDTH) * i, COLUMN_BLOCK_Y - (columnHeight + GAP * 2), BAR_WIDTH, columnHeight);
-    ctx.fillStyle = "#000";
+    ctx.fillStyle = '#000';
     ctx.fillText(names[i], COLUMN_BLOCK_X + (BAR_GAP + BAR_WIDTH) * i, COLUMN_BLOCK_Y);
     console.log(times[i]);
   }
